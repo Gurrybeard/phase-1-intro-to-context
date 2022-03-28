7cf087083da72cf8210135832431ce354b809d08
Benjamin <bboksner@gmail.com>
2022-03-28 18:38:19 -0400

    commit tree 7568e5f58874ac27901a185247ef63d0bdcbee14

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function createEmployeeRecord(array){
+ return {
+     firstName: array[0],
+     familyName:array[1],
+     title:array[2],
+     payPerHour:array[3],
+     timeInEvents:[],
+     timeOutEvents:[]
+    }
+}
+function createEmployeeRecords(arOfAr){
+    const records = [];
+   arOfAr.map((element,index) => {
+        records.push(createEmployeeRecord(arOfAr[index])) 
+    });
+    return records;
+}
+function createTimeInEvent(employeeRecord,dateTime){
+    let [date,hour] =dateTime.split(' ');
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour,10),
+        date
+    })
+    return employeeRecord;
+}
+function createTimeOutEvent(employeeRecord,dateTime){
+    let [date,hour] =dateTime.split(' ');
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour,10),
+        date
+    })
+    return employeeRecord;
+
+}
+
